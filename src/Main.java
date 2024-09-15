55bf6e699a54f1679f64501c2e93bf6b1888a938
Chris Stewart <cs6505@outlook.com>
2024-09-15 18:53:39 -0400

    added class and small func

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<User> myUserList = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        User user1 = new User("John", "Doe", false, false, false);
+        myUserList.add(user1);
+
+        User user2 = new User("Bob", "Doe", true, true, true);
+        myUserList.add(user2);
+
+        User user3 = new User("Frank", "Doe", false, true, true);
+        myUserList.add(user3);
+
+        for (int i = 0; i < myUserList.size(); i++)
+        {
+            printUserDiscount(myUserList.get(i));
         }
     }
+
+    public static class User
+    {
+        // constructor
+        public User(String fn, String ln, Boolean senior, Boolean firstResponder, Boolean military)
+        {
+            this.FirstName = fn;
+            this.LastName = ln;
+            this.IsSenior = senior;
+            this.IsFirstResponder = firstResponder;
+            this.IsMilitary = military;
+        }
+        public String FirstName;
+        public String LastName;
+        public Boolean IsSenior;
+        public Boolean IsFirstResponder;
+        public Boolean IsMilitary;
+    }
+
+    public static void printUserDiscount(User user)
+    {
+        int DiscountAmount = 0;
+
+        if(user.IsSenior)
+        {
+            DiscountAmount += 15;
+        }
+
+        if(user.IsFirstResponder)
+        {
+            DiscountAmount += 10;
+        }
+
+        if(user.IsMilitary)
+        {
+            DiscountAmount += 12;
+        }
+
+        System.out.println(user.FirstName + ", " + user.LastName + ", " + DiscountAmount + "% Discount \n");
+    }
 }
+
+
